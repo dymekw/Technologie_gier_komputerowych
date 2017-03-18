6c73ecd5801ff64f855775d4ba83fc26cbad7d36
Wojciech <wojtdym@interia.pl>
2017-03-18 18:46:27 +0100

    initial commit in KingKong project

---
+++ KingKong/Assets/CollistionDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollistionDetector : MonoBehaviour {
+
+    public GameObject wall;
+    public GameObject character;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == character.name)
+        {
+            Destroy(wall);
+        }
+        
+    }
+}
