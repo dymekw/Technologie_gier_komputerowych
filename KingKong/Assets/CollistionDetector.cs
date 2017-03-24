ad085b94e4ea47b935187754102976d3801ded80
Wojciech <wojtdym@interia.pl>
2017-03-24 17:42:20 +0100

    fixed script

--- KingKong/Assets/CollistionDetector.cs
+++ KingKong/Assets/CollistionDetector.cs
@@ -10,8 +10,11 @@ public class CollistionDetector : MonoBehaviour {
     private Vector3 offset = new Vector3(0, 0.01f, 0);
     private Vector3 rotation = new Vector3(1f, 1f, 1f);
 
+    public Renderer rend;
+
     // Use this for initialization
     void Start () {
+        rend = GetComponent<Renderer>();
     }
 	
 	// Update is called once per frame
@@ -24,9 +27,6 @@ public class CollistionDetector : MonoBehaviour {
         if (col.gameObject.name != character.name)
             return;
 
-        Debug.Log(getCollisionPoint(col));
-        Debug.Log(howShouldBeSplitted(col));
-
         if (shouldBeWholeDestroyed())
         {
             GameObject clone = Instantiate(destroyedWall, transform.position + offset, transform.rotation);
@@ -35,21 +35,24 @@ public class CollistionDetector : MonoBehaviour {
         }
         else
         {
-            float holeWidth = 0.2f;
+            float holeWidth = 1f / Mathf.Max(transform.lossyScale.x, transform.lossyScale.z);
 
             float collisionPoint = getCollisionPoint(col);
             float normalizedCollisionPoint = collisionPoint;
 
+            bool axis = splitVertically(col);
+            Debug.Log("Vertical: " + axis);
+
             if (collisionPoint - holeWidth > -0.5f)
             {
-                instansiateWall(-0.5f, collisionPoint - holeWidth);
+                instansiateWall(-0.5f, collisionPoint - holeWidth, axis);
             } else
             {
                 normalizedCollisionPoint = -0.5f + holeWidth;
             }
             if (collisionPoint + holeWidth < 0.5f)
             {
-                instansiateWall(collisionPoint + holeWidth, 0.5f);
+                instansiateWall(collisionPoint + holeWidth, 0.5f, axis);
             } else
             {
                 normalizedCollisionPoint = 0.5f - holeWidth;
@@ -58,7 +61,7 @@ public class CollistionDetector : MonoBehaviour {
             float holeBegin = normalizedCollisionPoint - holeWidth;
             float holeEnd = normalizedCollisionPoint + holeWidth;
 
-            instansiateDestroyedWall(holeBegin, holeEnd);
+            instansiateDestroyedWall(holeBegin, holeEnd, axis);
         }
 
         Destroy(gameObject);       
@@ -79,30 +82,60 @@ public class CollistionDetector : MonoBehaviour {
         return v.z;
     }
 
-    char howShouldBeSplitted(Collision col)
+    bool splitVertically(Collision col)
     {
         Vector3 v = transform.InverseTransformPoint(col.contacts[0].point);
 
-        if (v.z == 0.5f || v.z == -0.5f)
+        if ((v.z == 0.5f || v.z == -0.5f) && transform.lossyScale.z <= transform.lossyScale.x)
         {
-            return 'V';
+            return true;
         }
-        return 'H';
+        if ((v.x == 0.5f || v.x == -0.5f) && transform.lossyScale.z <= transform.lossyScale.x)
+        {
+            return true;
+        }
+        return false;
     }
 
-    GameObject instansiateWall(float begin, float end)
+    GameObject instansiateWall(float begin, float end, bool splitVer)
     {
-        Vector3 position = new Vector3(gameObject.transform.lossyScale.x * (end + begin) / 2, 0, 0);
-        Vector3 scale = new Vector3((end - begin) * gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, gameObject.transform.lossyScale.z);
+        Vector3 position;
+        Vector3 scale;
+
+        if (splitVer)
+        {
+            position = new Vector3(gameObject.transform.lossyScale.x * (end + begin) / 2, 0, 0);
+            scale = new Vector3((end - begin) * gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, gameObject.transform.lossyScale.z);
+        } else
+        {
+            position = new Vector3(0,0, gameObject.transform.lossyScale.z * (end + begin) / 2);
+            scale = new Vector3(gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, (end - begin) * gameObject.transform.lossyScale.z);
+        }
+
         GameObject newWall = Instantiate(wall, transform.position + position, transform.rotation);
         newWall.transform.localScale = scale;
+
+        rend.material.SetTextureScale("_MainTex", scale);
+
         return newWall;
     }
 
-    GameObject instansiateDestroyedWall(float begin, float end)
+    GameObject instansiateDestroyedWall(float begin, float end, bool splitVer)
     {
-        Vector3 position = new Vector3(gameObject.transform.lossyScale.x * (end + begin) / 2, 0, 0);
-        Vector3 scale = new Vector3((end - begin) * gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, gameObject.transform.lossyScale.z);
+        Vector3 position;
+        Vector3 scale;
+
+        if (splitVer)
+        {
+            position = new Vector3(gameObject.transform.lossyScale.x * (end + begin) / 2, 0, 0);
+            scale = new Vector3((end - begin) * gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, gameObject.transform.lossyScale.z);
+        }
+        else
+        {
+            position = new Vector3(0, 0, gameObject.transform.lossyScale.z * (end + begin) / 2);
+            scale = new Vector3(gameObject.transform.lossyScale.x, gameObject.transform.lossyScale.y, (end - begin) * gameObject.transform.lossyScale.z);
+        }
+
         GameObject newDestroyedWall = Instantiate(destroyedWall, transform.position + position + offset, transform.rotation);
         newDestroyedWall.transform.localScale = scale;
         newDestroyedWall.transform.Rotate(rotation);
