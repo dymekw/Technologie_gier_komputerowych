74f1d24608922b9985b8a29d2303decbfc01c521
michal <gitaramen@gmail.com>
2017-04-05 22:18:03 +0200

    add troll

---
+++ KingKong/Assets/TrollController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrollController : MonoBehaviour {
+
+    private float speed = 10.0F;
+    private float rotationSpeed = 100.0F;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translationV = Input.GetAxis("Vertical") * speed;
+        float translationH = Input.GetAxis("Horizontal") * speed;
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        translationV *= Time.deltaTime;
+        translationH *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(translationH, 0, translationV);
+        transform.Rotate(0, rotation, 0);
+    }
+}
