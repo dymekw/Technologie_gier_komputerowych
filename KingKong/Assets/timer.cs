1f0dbce5ad42344f2e28afcacd90cefabcb5a40b
Wojciech <wojtdym@interia.pl>
2017-04-06 22:42:11 +0200

    added canvas and timer

---
+++ KingKong/Assets/timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public Text text;
+    private float startTime;
+    private float maxTime = 120;
+
+	// Use this for initialization
+	void Start () {
+        startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timeLeft = maxTime - Time.time + startTime;
+        text.text = Mathf.Max(0,timeLeft).ToString("0.0");
+	}
+}
