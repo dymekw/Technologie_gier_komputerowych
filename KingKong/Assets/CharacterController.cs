18c27c12b96b34ddb4f4d521e4f49b7098967b6c
michal <gitaramen@gmail.com>
2017-06-08 22:10:25 +0200

    chcon

--- KingKong/Assets/CharacterController.cs
+++ KingKong/Assets/CharacterController.cs
@@ -20,7 +20,7 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetMouseButton(0))
+		if (Input.GetMouseButton(0) && !ExampleGUIAspectsController.isBlocked())
         {
             anim.Play("Attack");
         }
